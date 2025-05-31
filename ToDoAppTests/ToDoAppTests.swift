d0ae37245cc39679eaa0d2c605195d175b3b6377
Ha Da Hyun <https://github.com/dahyun27>
2025-05-31 19:28:29 +0900

    first commit

---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by 하다현 on 5/31/25.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
