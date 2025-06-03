96da6d46f24c764ce861f0d5c0b612a0232a4220
Ha Da Hyun <https://github.com/dahyun27>
2025-06-03 22:50:11 +0900

    Update DetailViewController

--- ToDoApp/Controllers/DetailViewController.swift
+++ ToDoApp/Controllers/DetailViewController.swift
@@ -7,21 +7,182 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+final class DetailViewController: UIViewController {
     
     @IBOutlet weak var redButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var purpleButton: UIButton!
     
+    // 버튼에 쉽게 접근하기 위해 배열로 만들어 놓기 (고차함수 사용 가능)
+    lazy var buttons: [UIButton] = { return [redButton, greenButton, blueButton, purpleButton] }()
     
+    @IBOutlet weak var mainTextView: UITextView!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    // 모델(저장 데이터를 관리하는 코어데이터)
+    let toDoManager = CoreDataManager.shared
+    
+    var toDoData: ToDoData? {
+        didSet {
+            temporaryNum = toDoData?.color
+        }
+    }
+    
+    // ToDo 색 구분을 위해 임시로 숫자저장하는 함수
+    // (나중에 어떤 색상이 선택되어 있는지 쉽게 파악하기 위해)
+    var temporaryNum: Int64? = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
+        
     }
     
+    func setup() {
+        mainTextView.delegate = self
+        
+        backgroundView.clipsToBounds = true
+        backgroundView.layer.cornerRadius = 10
+        
+        saveButton.clipsToBounds = true
+        saveButton.layer.cornerRadius = 8
+        
+        clearButtonColors()
+    }
+    
+    func configureUI() {
+        // 기존 데이터가 있을 때
+        if let toDoData = self.toDoData {
+            self.title = "메모 수정하기"
+            guard let text = toDoData.memoText else { return }
+            mainTextView.text = text
+            mainTextView.textColor = .black
+            saveButton.setTitle("UPDATE", for: .normal)
+            mainTextView.becomeFirstResponder()
+            
+            let color = MyColor(rawValue: toDoData.color)
+            setupColorTheme(color: color)
+        }
+        // 기존 데이터가 없을 때
+        else {
+            self.title = "새로운 메모 작성하기"
+            mainTextView.text = "새로운 메모를 여기에 입력하세요."
+            mainTextView.textColor = .lightGray
+            setupColorTheme(color: .red)
+        }
+        setupColorButton(num: temporaryNum ?? 1)
+    }
+    
+    // 버튼 둥글게 깍기 위한 정확한 시점
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // 모든 버튼에 설정 변경
+        buttons.forEach { button in
+            button.clipsToBounds = true
+            button.layer.cornerRadius = button.bounds.height / 2
+        }
+    }
+    
+    @IBAction func colorButtonTapped(_ sender: UIButton) {
+        // 임시숫자 저장
+        self.temporaryNum = Int64(sender.tag)
+        let color = MyColor(rawValue: Int64(sender.tag))
+        setupColorTheme(color: color)
+        
+        clearButtonColors()
+        setupColorButton(num: Int64(sender.tag))
+    }
 
     
+    // 텍스트뷰/저장(업데이트)버튼 색상 설정
+    func setupColorTheme(color: MyColor? = .red) {
+        backgroundView.backgroundColor = color?.backgoundColor
+        saveButton.backgroundColor = color?.backgoundColor
+    }
+
+    // 버튼 색상 새롭게 셋팅
+    func clearButtonColors() {
+        redButton.backgroundColor = MyColor.red.backgoundColor
+        redButton.setTitleColor(MyColor.red.buttonColor, for: .normal)
+        greenButton.backgroundColor = MyColor.green.backgoundColor
+        greenButton.setTitleColor(MyColor.green.buttonColor, for: .normal)
+        blueButton.backgroundColor = MyColor.blue.backgoundColor
+        blueButton.setTitleColor(MyColor.blue.buttonColor, for: .normal)
+        purpleButton.backgroundColor = MyColor.purple.backgoundColor
+        purpleButton.setTitleColor(MyColor.purple.buttonColor, for: .normal)
+    }
+    
+    
+    // 눌려진 버튼 색상 설정
+    func setupColorButton(num: Int64) {
+        switch num {
+        case 1:
+            redButton.backgroundColor = MyColor.red.buttonColor
+            redButton.setTitleColor(.white, for: .normal)
+        case 2:
+            greenButton.backgroundColor = MyColor.green.buttonColor
+            greenButton.setTitleColor(.white, for: .normal)
+        case 3:
+            blueButton.backgroundColor = MyColor.blue.buttonColor
+            blueButton.setTitleColor(.white, for: .normal)
+        case 4:
+            purpleButton.backgroundColor = MyColor.purple.buttonColor
+            purpleButton.setTitleColor(.white, for: .normal)
+        default:
+            redButton.backgroundColor = MyColor.red.buttonColor
+            redButton.setTitleColor(.white, for: .normal)
+        }
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        // 기존데이터가 있을때 ===> 기존 데이터 업데이트
+        if let toDoData = self.toDoData {
+            toDoData.memoText = mainTextView.text
+            toDoData.color = temporaryNum ?? 1
+            toDoManager.updateToDo(newToDoData: toDoData) {
+                print("업데이트 완료👍")
+                // 다시 전화면으로 돌아가기
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+        // 기존데이터가 없을때 ===> 새로운 데이터 생성
+        else {
+            let memoText = mainTextView.text
+            toDoManager.saveToDoData(toDoText: memoText, colorInt: temporaryNum ?? 1) {
+                print("저장 완료👍")
+                // 다시 전화면으로 돌아가기
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    // 다른 곳 터치하면 키보드 화면 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+}
+
+extension DetailViewController: UITextViewDelegate {
+    // 입력을 시작할때
+    // (텍스트뷰는 플레이스홀더가 따로 있지 않아서, 플레이스 홀더처럼 동작하도록 직접 구현)
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "텍스트를 여기에 입력하세요." {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+    
+    // 입력이 끝났을때
+    func textViewDidEndEditing(_ textView: UITextView) {
+        // 비어있으면 다시 플레이스 홀더처럼 입력하기 위해서 조건 확인
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = "텍스트를 여기에 입력하세요."
+            textView.textColor = .lightGray
+        }
+    }
 }
